6c34d0a019e3aecf4c643f7f9ba8088779610e93
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-14 09:55:05 +0200

    Added script info and GNU license declaration

--- online_lda_wsdl.py
+++ online_lda_wsdl.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+
+# online_lda_wsdl.py: This script uses online VB for LDA to
+# analyze a bunch of text files whose content has been extracted
+# from SOAP API descriptors (WSDLs). The script is based on
+# Matthew D. Hoffman's 'onlinewikipedia.py'
+#
+# Copyright (C) 2013  Leandro Ordonez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import cPickle, string, numpy, getopt, sys, random, time, re, pprint, os
 
 import onlineldavb
