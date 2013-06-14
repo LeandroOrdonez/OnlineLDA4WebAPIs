b79c3ccf7b2fa32f546f9857c0737113dc7c610a
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-14 14:47:57 +0200

    Simple octave script for displaying the per-document topic distributions of the first 40 text files analized by the online LDA algorithm.

--- plot_topic_distributions.m
+++ plot_topic_distributions.m
@@ -4,5 +4,6 @@ function plot_topic_distributions()
     for i = 1:40
         subplot(5,8,i)
         plot(gamma(i,:)./sum(gamma(i,:)))
+        title(cstrcat("Operation ", num2str(i)))
     end
 endfunction
