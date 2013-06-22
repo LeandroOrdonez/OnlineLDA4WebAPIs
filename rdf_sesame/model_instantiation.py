ba9e88c98e8a829c86added82de93f54d063ccec
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-22 23:24:45 +0200

    Update: quotes under new_operation() and new_category() methods fixed

--- rdf_sesame/model_instantiation.py
+++ rdf_sesame/model_instantiation.py
@@ -29,7 +29,7 @@ def new_membership_relation(membership_relation_id, membership_probability, cate
 
 def new_operation(operation_id, membership_relations):
     memberships = ''.join(['<is_member_of rdf:resource="http://www.example.org/membership_relations/%s"/>\n' \
-% `membership` for membership in membership_relations])
+% membership for membership in membership_relations])
     return """<Operation rdf:about="http://www.example.org/operations/%s">
     %s  
     </Operation>""" % (operation_id, memberships)
@@ -42,7 +42,7 @@ def new_term_relation(term_relation_id, term_probability, term_value):
 
 def new_category(category_id, term_relations):
     terms = ''.join(['<has_term rdf:resource="http://www.example.org/term_relations/%s"/>\n' \
-% `term` for term in term_relations])
+% term for term in term_relations])
     return """<Category rdf:about="http://www.example.org/categories/%s">
     %s  
     </Category>""" % (category_id, terms)
