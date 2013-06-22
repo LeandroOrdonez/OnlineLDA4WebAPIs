15a7867bf16fdc9af5058a5bf0ee5ea6f45d8f48
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-22 22:17:58 +0200

    Update model_instantiation.py

--- rdf_sesame/model_instantiation.py
+++ rdf_sesame/model_instantiation.py
@@ -1,3 +1,21 @@
+# model_instantiation.py: Package of functions for creating RDF statements
+# according to the model defined in web_api_model.rdf
+#
+# Copyright (C) 2013 Leandro Ordonez
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 def new_term(term_id, term):
     return """<Term rdf:about="http://www.example.org/terms/%s">
     <has_content rdf:datatype="http://www.w3.org/2001/XMLSchema#string">%s</has_content>
